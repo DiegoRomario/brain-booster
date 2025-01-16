495eb921169eb43c8911d9a3999682da64e1d64b
DiegoRomario <diego.romario@outlook.com>
2025-01-16 06:56:00 -0300

    https://leetcode.com/problems/two-sum

---
+++ src/leetcode/01TwoSum.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode;
+internal static partial class Problem
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        Dictionary<int, int> visitedNumbers = [];
+
+        for (int index = 0; index < nums.Length; index++)
+        {
+            var x = target - nums[index];
+
+            if (visitedNumbers.TryGetValue(x, out int value)) return [index, value];
+
+            visitedNumbers[nums[index]] = index;
+        }
+
+        return [];
+    }
+}
---
+++ src/leetcode/Program.cs
@@ -0,0 +1,5 @@
+﻿
+
+using leetcode;
+
+Problem.TwoSum([1, 2, 4, 8, 20], 12);
