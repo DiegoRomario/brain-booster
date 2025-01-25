787663fed32961a6cc2cee268639a9360368e018
DiegoRomario <diego.romario@outlook.com>
2025-01-25 18:04:53 -0300

    https://leetcode.com/problems/remove-duplicates-from-sorted-array

---
+++ src/leetcode/07RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode;
+
+internal static partial class Problem
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0) return 0;
+
+        int lastUniqueIndex = 0;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[lastUniqueIndex])
+            {
+                lastUniqueIndex++; // Move to the next position for the unique element
+                nums[lastUniqueIndex] = nums[i]; // Place the unique element
+            }
+        }
+
+        return lastUniqueIndex + 1; // The count of unique elements
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,20 +1,6 @@
 ﻿
 
 using leetcode;
-using static leetcode.Problem;
 
-var item1 = new ListNode(10);
-var item2 = new ListNode(5, item1);
-var item3 = new ListNode(1, item2);
-
-var item4 = new ListNode(7);
-var item5 = new ListNode(5, item4);
-var item6 = new ListNode(3, item5);
-
-var result = Problem.MergeTwoLists(item3, item6);
-
-while (result != null)
-{
-    Console.WriteLine(result.val);
-    result = result.next;
-}
+var result = Problem.RemoveDuplicates([1, 1, 2]);
+Console.WriteLine(result);
