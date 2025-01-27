8013a43fff607fda52682bb56109c0451491b97e
DiegoRomario <diego.romario@outlook.com>
2025-01-27 20:41:03 -0300

    https://leetcode.com/problems/remove-element

---
+++ src/leetcode/08RemoveElement.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode;
+
+internal static partial class Problem
+{
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int validElements = 0; // Pointer for the position of the next valid element
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[validElements] = nums[i]; // Move valid element to the current position of validElements
+                validElements++; // Increment the count of valid elements
+            }
+        }
+
+        return validElements;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -2,5 +2,6 @@
 
 using leetcode;
 
-var result = Problem.RemoveDuplicates([1, 1, 2]);
+int[] nums = [0, 1, 2, 2, 3, 0, 4, 2];
+var result = Problem.RemoveElement(nums, 2);
 Console.WriteLine(result);
