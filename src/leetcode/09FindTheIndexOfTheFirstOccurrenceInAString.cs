e69a54d7f9a413e3311b2a68cd2afa2311b2a51f
DiegoRomario <diego.romario@outlook.com>
2025-01-29 19:15:35 -0300

    https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string

---
+++ src/leetcode/09FindTheIndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode;
+
+internal static partial class Problem
+{
+    public static int StrStr(string haystack, string needle)
+    {
+        int needleSize = needle.Length;
+        int haystackSize = haystack.Length;
+
+        for (int haystackIndex = 0; haystackIndex <= haystackSize - needleSize; haystackIndex++)
+        {
+            for (int needleIndex = 0; needleIndex < needleSize; needleIndex++)
+            {
+                if (needle[needleIndex] != haystack[haystackIndex + needleIndex]) break;
+
+                if (needleIndex == needleSize - 1) return haystackIndex;
+            }
+        }
+
+        return -1;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -2,6 +2,5 @@
 
 using leetcode;
 
-int[] nums = [0, 1, 2, 2, 3, 0, 4, 2];
-var result = Problem.RemoveElement(nums, 2);
+var result = Problem.StrStr("mississippi", "issip");
 Console.WriteLine(result);
