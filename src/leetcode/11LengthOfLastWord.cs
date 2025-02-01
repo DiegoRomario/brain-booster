91ae58ff2e80cd699c0d92c18fc80de86e9545d0
DiegoRomario <diego.romario@outlook.com>
2025-02-01 15:54:06 -0300

    https://leetcode.com/problems/length-of-last-word

---
+++ src/leetcode/11LengthOfLastWord.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode;
+
+internal static partial class Problem
+{
+    public static int LengthOfLastWord(string s)
+    {
+        int length = 0;
+        int lastCharIndex = s.Length - 1;
+
+        // Skip trailing spaces
+        while (lastCharIndex >= 0 && s[lastCharIndex] == ' ')
+        {
+            lastCharIndex--;
+        }
+
+        // Count the length of the last word
+        while (lastCharIndex >= 0 && s[lastCharIndex] != ' ')
+        {
+            length++;
+            lastCharIndex--;
+        }
+
+        return length;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -2,5 +2,5 @@
 
 using leetcode;
 
-var result = Problem.SearchInsert([1, 3, 5, 6, 7, 8, 9, 44, 55, 77], 55);
+var result = Problem.LengthOfLastWord("Hello World");
 Console.WriteLine(result);
