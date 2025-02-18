0fa1336f64277e21817c9b8d590b99e232623f25
DiegoRomario <diego.romario@outlook.com>
2025-02-18 07:29:44 -0300

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock

---
+++ src/leetcode/16BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,23 @@
+﻿internal static partial class Problem
+{
+    public static int MaxProfit(int[] prices)
+    {
+        if (prices.Length < 2) return 0;
+
+        int max = 0;
+        int minPrice = prices[0];
+
+        for (int i = 1; i < prices.Length; i++) // Start from 1, since 0 is already considered
+        {
+            if (prices[i] < minPrice)
+                minPrice = prices[i];
+
+            int profit = prices[i] - minPrice;
+
+            if (profit > max)
+                max = profit;
+        }
+
+        return max;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,2 @@
-﻿var result = Problem.ClimbStairs(144);
+﻿var result = Problem.MaxProfit([3, 8, 5, 10, 1, 2]);
 Console.WriteLine(result);
