7796b3cf3805f01cc2a7c9ae8b8be7f014beedb1
DiegoRomario <diego.romario@outlook.com>
2025-03-01 17:47:28 -0300

    https://leetcode.com/problems/binary-tree-inorder-traversal

---
+++ src/leetcode/18BinaryTreeInorderTraversal.cs
@@ -0,0 +1,49 @@
+﻿internal static partial class Problem
+{
+    public static IList<int> InorderTraversal(TreeNode root)
+    {
+        List<int> result = [];
+        Stack<TreeNode> stack = [];
+        TreeNode current = root;
+
+        while (current != null || stack.Count > 0)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+
+            current = stack.Pop();
+            result.Add(current.val);
+            current = current.right;
+        }
+
+        return result;
+    }
+
+    public static IList<int> InorderTraversalRecursion(TreeNode root)
+    {
+        var vals = new List<int>();
+        InOrderHelper(root, vals);
+        return vals;
+
+        static void InOrderHelper(TreeNode root, List<int> vals)
+        {
+            if (root == null) return;
+            InOrderHelper(root.left, vals);
+            vals.Add(root.val);
+            InOrderHelper(root.right, vals);
+        }
+    }
+
+
+    public class TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        public int val = val;
+        public TreeNode left = left;
+        public TreeNode right = right;
+    }
+}
+
+
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,22 @@
-﻿Problem.Merge([1, 8, 9, 0, 0, 0], 3, [2, 5, 6], 3);
-//Console.WriteLine(result);
+﻿using static Problem;
+
+TreeNode root = new TreeNode(1,
+    new TreeNode(2,
+        new TreeNode(4),
+        new TreeNode(5,
+            new TreeNode(6),
+            new TreeNode(7)
+        )
+    ),
+    new TreeNode(3,
+        null,
+        new TreeNode(8,
+            new TreeNode(9),
+            null
+        )
+    )
+);
+
+
+Problem.InorderTraversal(root);
+Problem.InorderTraversalRecursion(root);
