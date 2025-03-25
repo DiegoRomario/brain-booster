8ae4a04a59717f4b0fd32c9947b70ff72bc1d34f
DiegoRomario <diego.romario@outlook.com>
2025-03-25 06:42:01 -0300

    https://frontendmasters.com/courses/algorithms/implementing-two-crystal-balls

---
+++ src/primeagen/01TwoCrystalBalls.cs
@@ -0,0 +1,24 @@
+﻿namespace primeagen;
+internal static partial class Problem
+{
+    public static int FindBreakingPoint(bool[] floors)
+    {
+        int jump = (int)Math.Floor(Math.Sqrt(floors.Length));
+
+        int i = jump;
+
+        for (; i < floors.Length; i += jump)
+        {
+            if (floors[i]) break;
+        }
+
+        i -= jump;
+
+        for (int j = 0; j <= jump && i < floors.Length; ++j, ++i)
+        {
+            if (floors[i]) return i;
+        }
+
+        return -1;
+    }
+}
---
+++ src/primeagen/Program.cs
@@ -0,0 +1,7 @@
+﻿using primeagen;
+
+bool[] floors =
+ [false, false, false, false, false, false, true, true, true, true];
+
+int result = Problem.FindBreakingPoint(floors);
+Console.WriteLine("Breaking floor: " + result); // Expected output: 6
