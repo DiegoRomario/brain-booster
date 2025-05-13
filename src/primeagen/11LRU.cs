2a3e5c0b8ee41499d01afda24553fc93cbcc0dc6
DiegoRomario <diego.romario@outlook.com>
2025-05-13 08:23:52 -0300

    https://frontendmasters.com/courses/algorithms/implementing-an-lru-cache

---
+++ src/primeagen/11LRU.cs
@@ -0,0 +1,96 @@
+﻿namespace primeagen
+{
+    public class Node<T>
+    {
+        public required T Value { get; set; }
+        public Node<T>? Prev { get; set; }
+        public Node<T>? Next { get; set; }
+    }
+
+    public class LRU<K, V>(int capacity) where K : notnull
+    {
+        private int _length;
+        private Node<V>? _head;
+        private Node<V>? _tail;
+        private readonly Dictionary<K, Node<V>> _lookup = [];
+        private readonly Dictionary<Node<V>, K> _reverseLookup = [];
+
+        public void Update(K key, V value)
+        {
+            if (!_lookup.TryGetValue(key, out var node))
+            {
+                node = new Node<V> { Value = value };
+                _length++;
+                Prepend(node);
+                TrimCache();
+
+                _lookup[key] = node;
+                _reverseLookup[node] = key;
+            }
+            else
+            {
+                Detach(node);
+                Prepend(node);
+                _reverseLookup.Remove(node);
+                node.Value = value;
+            }
+        }
+
+        public V? Get(K key)
+        {
+            if (!_lookup.TryGetValue(key, out var node))
+                return default;
+
+            Detach(node);
+            Prepend(node);
+            return node.Value;
+        }
+
+        private void Detach(Node<V> node)
+        {
+            if (node.Prev is not null)
+                node.Prev.Next = node.Next;
+
+            if (node.Next is not null)
+                node.Next.Prev = node.Prev;
+
+            if (_length == 1)
+                (_head, _tail) = (null, null);
+
+            if (_head == node)
+                _head = node.Next;
+
+            if (_tail == node)
+                _tail = node.Prev;
+
+            node.Next = node.Prev = null;
+        }
+
+        private void Prepend(Node<V> node)
+        {
+            if (_head is null)
+            {
+                _head = _tail = node;
+                return;
+            }
+
+            node.Next = _head;
+            _head.Prev = node;
+            _head = node;
+        }
+
+        private void TrimCache()
+        {
+            if (_length <= capacity)
+                return;
+
+            var tail = _tail!;
+            Detach(tail);
+
+            var key = _reverseLookup[tail];
+            _lookup.Remove(key);
+            _reverseLookup.Remove(tail);
+            _length--;
+        }
+    }
+}
--- src/primeagen/Program.cs
+++ src/primeagen/Program.cs
@@ -1,30 +1,11 @@
-﻿using primeagen;
+﻿
+using primeagen;
 
+var lru = new LRU<string, int>(3);
 
-var graph = BuildGraph();
-var path = Dijkstra.DijkstraList(0, 6, graph);
+lru.Update("foo", 69);
+lru.Update("bar", 420);
+lru.Update("baz", 1337);
+lru.Update("ball", 69420);
 
-if (path == null)
-    Console.WriteLine("No path found.");
-else
-    Console.WriteLine(string.Join(" -> ", path));
-
-
-static List<GraphEdge>[] BuildGraph()
-{
-    var list = new List<GraphEdge>[7];
-
-    for (int i = 0; i < list.Length; i++)
-        list[i] = [];
-
-    list[0].AddRange([new GraphEdge(1, 3), new GraphEdge(2, 1)]);
-    list[1].AddRange([new GraphEdge(0, 3), new GraphEdge(2, 4), new GraphEdge(4, 1)]);
-    list[2].AddRange([new GraphEdge(0, 1), new GraphEdge(1, 4)]);
-    list[3].AddRange([new GraphEdge(4, 5), new GraphEdge(6, 1)]);
-    list[4].AddRange([new GraphEdge(1, 1), new GraphEdge(3, 5), new GraphEdge(5, 2)]);
-    list[5].AddRange([new GraphEdge(4, 2), new GraphEdge(6, 1)]);
-    list[6].AddRange([new GraphEdge(3, 1), new GraphEdge(5, 1)]);
-
-    return list;
-}
-
+Console.WriteLine(lru.Get("foo"));
