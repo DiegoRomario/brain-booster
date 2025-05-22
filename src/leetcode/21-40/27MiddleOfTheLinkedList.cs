39e8fc4a0ec674c67bcfc01f7dd8a2bff47b09cf
DiegoRomario <diego.romario@outlook.com>
2025-05-22 07:59:21 -0300

    https://leetcode.com/problems/middle-of-the-linked-list

---
+++ src/leetcode/21-40/27MiddleOfTheLinkedList.cs
@@ -0,0 +1,18 @@
+﻿using static leetcode.Problem;
+
+internal static partial class Problem
+{
+    public static ListNode MiddleNode(ListNode head)
+    {
+        var nodes = new ListNode[100];
+        var index = 0;
+
+        while (head != null)
+        {
+            nodes[index++] = head;
+            head = head.next;
+        }
+
+        return nodes[index / 2];
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,5 +1,5 @@
 ﻿
 using static leetcode.Problem;
 
-var head = new ListNode(1, new ListNode(2, new ListNode(3)));
-Problem.ReverseListRecursive(head);
+var head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))));
+Problem.MiddleNode(head);
