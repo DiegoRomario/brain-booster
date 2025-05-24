64d2d5041e6a7340f541da386e6f477734ee9522
DiegoRomario <diego.romario@outlook.com>
2025-05-24 16:23:32 -0300

    eetcode.com/problems/range-sum-of-bst

---
+++ src/leetcode/21-40/31RangeSumOfBST.cs
@@ -0,0 +1,24 @@
+﻿internal static partial class Problem
+{
+    public static int RangeSumBST(TreeNode root, int low, int high)
+    {
+        int total = Recursion(root, low, high, 0);
+        return total;
+    }
+
+    public static int Recursion(TreeNode root, int low, int high, int total)
+    {
+        if (root is null) return total;
+
+        if (root.val >= low && root.val <= high)
+        {
+            total += root.val;
+        }
+
+        total = root.val >= low ? Recursion(root.left, low, high, total) : total;
+        total = root.val <= high ? Recursion(root.right, low, high, total) : total;
+
+        return total;
+    }
+}
+
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,6 +1,15 @@
 ﻿
-using static leetcode.Problem;
+using static Problem;
 
-
-var result = Problem.TotalNumbers([9, 5, 2]);
+TreeNode root = new(10,
+       new TreeNode(5,
+           new TreeNode(3),
+           new TreeNode(7)
+       ),
+       new TreeNode(15,
+           null,
+           new TreeNode(18)
+       )
+   );
+var result = RangeSumBST(root,7,15);
 Console.ReadKey();
