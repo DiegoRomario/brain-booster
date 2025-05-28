5e2674e82d5ccfba8413de07dc43dae2a6fd2e5b
DiegoRomario <diego.romario@outlook.com>
2025-05-28 20:53:13 -0300

    https://leetcode.com/problems/invert-binary-tree

---
+++ src/leetcode/21-40/33InvertBinaryTree.cs
@@ -0,0 +1,15 @@
+﻿internal static partial class Problem
+{
+    public static TreeNode InvertTree(TreeNode root)
+    {
+        if (root is not null)
+        {
+            var temp = root.left;
+            root.left = InvertTree(root.right);
+            root.right = InvertTree(temp);
+        }
+        return root;
+    }
+}
+
+
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,15 +1,23 @@
 ﻿
 using static Problem;
 
-TreeNode root = new(10,
-       new TreeNode(5,
-           new TreeNode(3),
-           new TreeNode(7)
-       ),
-       new TreeNode(15,
-           null,
-           new TreeNode(18)
-       )
-   );
-var result = RangeSumBST(root,7,15);
+// Level 3
+TreeNode leftLeft = new TreeNode(4);
+TreeNode leftRight = new TreeNode(5);
+TreeNode rightLeft = new TreeNode(6);
+TreeNode rightRight = new TreeNode(7);
+
+// Level 2
+TreeNode left = new TreeNode(2, leftLeft, leftRight);
+TreeNode right = new TreeNode(3, rightLeft, rightRight);
+
+// Level 1 (Root)
+TreeNode root = new TreeNode(1, left, right);
+var result = InvertTree(root);
 Console.ReadKey();
+
+
+//TreeNode node3 = new TreeNode(3);
+//TreeNode node2 = new TreeNode(2, null, node3);
+//TreeNode node1 = new TreeNode(1, null, node2);
+//TreeNode root = new TreeNode(4, null, node1);
