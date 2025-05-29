6747567a68c65de66677c6a5c9854ce2a64b4d0e
DiegoRomario <diego.romario@outlook.com>
2025-05-29 20:24:17 -0300

    eetcode.com/problems/find-center-of-star-graph

---
+++ src/leetcode/21-40/34FindCenterOfStarGraph.cs
@@ -0,0 +1,9 @@
+﻿internal static partial class Problem
+{
+    public static int FindCenter(int[][] edges)
+    {
+        return (edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1]) ? edges[0][0] : edges[0][1];
+    }
+}
+
+
