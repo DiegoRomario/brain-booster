c633ce1d38cbe58a8ba3538181bfebfd06c14398
DiegoRomario <diego.romario@outlook.com>
2025-06-05 20:46:53 -0300

    https://leetcode.com/problems/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance

---
+++ src/leetcode/21-40/37FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.cs
@@ -0,0 +1,79 @@
+﻿
+
+internal static partial class Problem
+{
+    public static int FindTheCity(int n, int[][] edges, int distanceThreshold)
+    {
+        // Build the graph as an adjacency list
+        var graph = new Dictionary<int, List<(int neighbor, int weight)>>();
+
+        for (int i = 0; i < n; i++) graph[i] = [];
+
+        foreach (var edge in edges)
+        {
+            int from = edge[0], to = edge[1], weight = edge[2];
+            graph[from].Add((to, weight));
+            graph[to].Add((from, weight));
+        }
+
+        int minReachable = int.MaxValue;
+        int resultCity = -1;
+
+        for (int city = 0; city < n; city++)
+        {
+            int reachableCount = Dijkstra(city);
+            if (reachableCount <= minReachable) // prefer larger index on tie
+            {
+                minReachable = reachableCount;
+                resultCity = city;
+            }
+        }
+
+        return resultCity;
+
+        int Dijkstra(int sourceCity)
+        {
+            int[] minDistance = new int[n];
+            Array.Fill(minDistance, int.MaxValue);
+            minDistance[sourceCity] = 0;
+
+            // priorityQueue holds pairs (city, distanceSoFar), ordered by distanceSoFar
+            var priorityQueue = new PriorityQueue<(int city, int distanceSoFar), int>();
+            priorityQueue.Enqueue((sourceCity, 0), 0);
+
+            while (priorityQueue.Count > 0)
+            {
+                var (currentCity, distanceSoFar) = priorityQueue.Dequeue();
+
+                // 1) Skip outdated entries
+                if (distanceSoFar > minDistance[currentCity]) continue;
+
+                // 2) No need to explore beyond the threshold
+                if (distanceSoFar > distanceThreshold) break;
+
+                foreach (var (neighborCity, edgeWeight) in graph[currentCity])
+                {
+                    int newDistance = distanceSoFar + edgeWeight;
+
+                    // Update only if we found a shorter path within the threshold
+                    if (newDistance < minDistance[neighborCity] && newDistance <= distanceThreshold)
+                    {
+                        minDistance[neighborCity] = newDistance;
+                        priorityQueue.Enqueue((neighborCity, newDistance), newDistance);
+                    }
+                }
+            }
+
+            int reachableCount = 0;
+            for (int city = 0; city < n; city++)
+            {
+                if (city != sourceCity && minDistance[city] <= distanceThreshold)
+                    reachableCount++;
+            }
+
+            return reachableCount;
+        }
+    }
+}
+
+
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,11 +1,6 @@
 ﻿
 using static Problem;
 
-var response = ValidPath(6, [[0, 1], [0, 2], [3, 5], [5, 4], [4, 3]], 0,5);
+var result = FindTheCity(4,[ [0, 1, 3], [1, 2, 1], [1, 3, 4], [2, 3, 1]], 4);
+Console.WriteLine(result);
 Console.ReadKey();
-
-
-//TreeNode node3 = new TreeNode(3);
-//TreeNode node2 = new TreeNode(2, null, node3);
-//TreeNode node1 = new TreeNode(1, null, node2);
-//TreeNode root = new TreeNode(4, null, node1);
