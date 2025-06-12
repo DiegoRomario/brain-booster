4708195cd7f3c3b9d70d9969819db316b5909e16
DiegoRomario <diego.romario@outlook.com>
2025-06-12 20:50:13 -0300

    https://leetcode.com/problems/lru-cache

--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,6 +1,10 @@
-﻿
-using static Problem;
+﻿var cache = new LRUCache(2);
 
-var result = AllPathsSourceTarget([[1, 2], [3], [3], []]);
-Console.WriteLine(result);
-Console.ReadKey();
+cache.Put(2, 1);
+cache.Put(2, 2);
+Console.WriteLine("Get(2): " + cache.Get(2)); // 2
+
+cache.Put(1, 1);
+cache.Put(4, 1);
+
+Console.WriteLine("Get(2): " + cache.Get(2)); // -1 (evicted)
