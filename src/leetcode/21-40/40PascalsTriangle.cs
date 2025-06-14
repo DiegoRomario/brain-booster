effef66246410bbd7db6702f3dcd3fbfadfc6cdb
DiegoRomario <diego.romario@outlook.com>
2025-06-14 09:52:41 -0300

    https://leetcode.com/problems/pascals-triangle

---
+++ src/leetcode/21-40/40PascalsTriangle.cs
@@ -0,0 +1,32 @@
+﻿internal static partial class Problem
+{
+    public static IList<IList<int>> Generate(int numRows)
+    {
+        List<IList<int>> triangle = [];
+
+        for (int i = 0; i < numRows; i++)
+        {
+            List<int> row = [];
+
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0 || j == i)
+                {
+                    row.Add(1);
+                }
+                else
+                {
+                    var sum = triangle[i - 1][j] + triangle[i - 1][j - 1];
+                    row.Add(sum);
+                }
+            }
+
+            triangle.Add(row);
+        }
+
+        return triangle;
+    }
+}
+
+
+
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,10 +1,1 @@
-﻿var cache = new LRUCache(2);
-
-cache.Put(2, 1);
-cache.Put(2, 2);
-Console.WriteLine("Get(2): " + cache.Get(2)); // 2
-
-cache.Put(1, 1);
-cache.Put(4, 1);
-
-Console.WriteLine("Get(2): " + cache.Get(2)); // -1 (evicted)
+﻿var result = Problem.Generate(5);
