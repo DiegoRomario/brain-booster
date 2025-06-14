66d01a7a196d9cf25f49d8d9acabe5d468c997d9
DiegoRomario <diego.romario@outlook.com>
2025-06-14 15:46:54 -0300

    https://leetcode.com/problems/house-robber

---
+++ src/leetcode/41-60/43HouseRobber.cs
@@ -0,0 +1,22 @@
+﻿internal static partial class Problem
+{
+    // [25, 30, 25, 1, 20, 20, 1, 25, 1, 50]
+    public static int Rob(int[] nums)
+    {
+        if (nums is null || nums.Length is 0) return 0;
+        if (nums.Length is 1) return nums[0];
+        if (nums.Length is 2) return Math.Max(nums[0], nums[1]);
+
+        int[] amount = new int[nums.Length];
+
+        amount[0] = nums[0];
+        amount[1] = Math.Max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.Length; i++)
+        {
+            amount[i] = Math.Max(nums[i] + amount[i - 2], amount[i -1]);
+        }
+
+        return amount[nums.Length - 1];
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,6 +1,2 @@
-﻿ListNode node4 = new ListNode(9);
-ListNode node3 = new ListNode(1) { next = node4 };
-ListNode node2 = new ListNode(5) { next = node3 };
-ListNode head = new ListNode(4) { next = node2 };
-Problem.DeleteNode(node2);
+﻿Problem.Rob([1, 2, 3, 1]);
 Console.ReadKey();
