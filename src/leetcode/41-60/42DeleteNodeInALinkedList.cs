3616db0b72854cff4feb5a202cc2dde1630328ff
DiegoRomario <diego.romario@outlook.com>
2025-06-14 11:08:54 -0300

    https://leetcode.com/problems/delete-node-in-a-linked-list

---
+++ src/leetcode/41-60/42DeleteNodeInALinkedList.cs
@@ -0,0 +1,18 @@
+﻿internal static partial class Problem
+{
+    public static void DeleteNode(ListNode node)
+    {
+        node.val = node.next.val;
+        node.next = node.next.next;       
+    }
+}
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
+
+
+
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,1 +1,6 @@
-﻿var result = Problem.BuildArray([0, 2, 1, 5, 3, 4]);
+﻿ListNode node4 = new ListNode(9);
+ListNode node3 = new ListNode(1) { next = node4 };
+ListNode node2 = new ListNode(5) { next = node3 };
+ListNode head = new ListNode(4) { next = node2 };
+Problem.DeleteNode(node2);
+Console.ReadKey();
