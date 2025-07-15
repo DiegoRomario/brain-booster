daf658bb58def9f977221d268034debdbcced9e1
DiegoRomario <diego.romario@outlook.com>
2025-07-15 07:52:39 -0300

    https://leetcode.com/problems/longest-consecutive-sequence

---
+++ src/leetcode/41-60/52LongestConsecutiveSequence.cs
@@ -0,0 +1,27 @@
+﻿internal static partial class Problem
+{
+    public static int LongestConsecutive(int[] nums)
+    {
+        Dictionary<int, bool> set = new(nums.Length);
+
+        for (int i = 0; i < nums.Length; i++)
+            set[nums[i]] = false;
+
+        int longestConsecutive = 0;
+
+        foreach ((int num, _) in set)
+        {
+            if (!set.ContainsKey(num - 1))
+            {
+                int consecutive = 1;
+                while (set.ContainsKey(num + consecutive))
+                    consecutive++;
+
+                if (consecutive > longestConsecutive)
+                    longestConsecutive = consecutive;
+            }
+        }
+
+        return longestConsecutive;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,2 @@
-﻿Problem.FindMaxAverage([1, 12, -5, -6, 50, 3],4);
+﻿Problem.LongestConsecutive([0,9,7,3,28,290,387,39,201,31,3,5]);
 Console.ReadKey();
