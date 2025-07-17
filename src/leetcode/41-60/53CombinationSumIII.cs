7a700c4bdc3433e75a3fb7894a7178d02a54ecbd
DiegoRomario <diego.romario@outlook.com>
2025-07-17 07:17:06 -0300

    https://leetcode.com/problems/combination-sum-iii

---
+++ src/leetcode/41-60/53CombinationSumIII.cs
@@ -0,0 +1,29 @@
+﻿internal static partial class Problem
+{
+    public static IList<IList<int>> CombinationSum3(int k, int n)
+    {
+        List<IList<int>> result = [];
+
+        Comb([], k, n, 1);
+        return result;
+
+        void Comb(List<int> curr, int remainingK, int remainingN, int start)
+        {
+            if (remainingK == 0 && remainingN == 0)
+            {
+                result.Add([.. curr]);
+                return;
+            }
+
+            if (remainingK <= 0)
+                return;
+
+            for (int i = start; i <= Math.Min(9, remainingN); i++)
+            {
+                curr.Add(i);
+                Comb(curr, remainingK - 1, remainingN - i, i + 1);
+                curr.RemoveAt(curr.Count - 1);
+            }
+        }
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,2 @@
-﻿Problem.LongestConsecutive([0,9,7,3,28,290,387,39,201,31,3,5]);
+﻿Problem.CombinationSum3(3,9);
 Console.ReadKey();
