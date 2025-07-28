e09fc6914c0e5f5a5943e2b9e59e7eff423fef82
DiegoRomario <diego.romario@outlook.com>
2025-07-28 09:51:56 -0300

    https://leetcode.com/problems/find-peak-element

---
+++ src/leetcode/61-80/64FindPeakElement.cs
@@ -0,0 +1,21 @@
+﻿internal static partial class Problem
+{
+    public static int FindPeakElement(int[] nums)
+    {
+        int low = 0;
+        int high = nums.Length - 1;     
+        
+        while (low < high)
+        {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] < nums[mid + 1])
+                low = mid + 1;
+            else
+                high = mid;
+                
+        }
+        
+        return low;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,2 @@
-﻿Problem.MatrixScore([[0, 0, 1, 1], [1, 0, 1, 0], [1, 1, 0, 0]]);
+﻿Problem.FindPeakElement([1, 2, 3, 1]);
 Console.ReadKey();
