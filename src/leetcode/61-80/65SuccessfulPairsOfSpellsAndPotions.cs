0bdd59480bb79befe8cd167912e08545bf262d09
DiegoRomario <diego.romario@outlook.com>
2025-07-28 16:09:59 -0300

    https://leetcode.com/problems/successful-pairs-of-spells-and-potions

---
+++ src/leetcode/61-80/65SuccessfulPairsOfSpellsAndPotions.cs
@@ -0,0 +1,32 @@
+﻿internal static partial class Problem
+{
+    public static int[] SuccessfulPairs(int[] spells, int[] potions, long success)
+    {
+        Array.Sort(potions);
+        int[] res = new int[spells.Length];
+
+        for (int i = 0; i < spells.Length; i++)
+        {
+            int low = 0;
+            int high = potions.Length;
+
+            while (low < high)
+            {
+                int mid = (low + high) / 2;
+
+                if ((long)spells[i] * potions[mid] >= success)
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            res[i] = potions.Length - low;
+
+        }
+        return res;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,2 @@
-﻿Problem.FindPeakElement([1, 2, 3, 1]);
+﻿Problem.SuccessfulPairs([5, 1, 3], [1, 2, 3, 4, 5],  7);
 Console.ReadKey();
