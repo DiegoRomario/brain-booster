42b560fc01e5cea57ea9dca1198460527b1d96b8
DiegoRomario <diego.romario@outlook.com>
2025-08-04 07:53:33 -0300

    https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length

---
+++ src/leetcode/61-80/71MaxVowels.cs
@@ -0,0 +1,19 @@
+﻿internal static partial class Problem
+{
+    public static int MaxVowels(string s, int k)
+    {
+        HashSet<char> vowels = ['a', 'e', 'i', 'o', 'u'];
+        int max = 0, count = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (vowels.Contains(s[i])) count++;
+            if (i >= k && vowels.Contains(s[i - k])) count--;
+            max = Math.Max(max, count);
+            if (max == k) return k; // Early exit optimization
+        }
+
+        return max;
+    }
+
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,2 @@
-﻿Problem.SearchMatrix([[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], 5);
+﻿Problem.MaxVowels("abciiidef", 3);
 Console.ReadKey();
