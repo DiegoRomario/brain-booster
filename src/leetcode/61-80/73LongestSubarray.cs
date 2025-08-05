50a440727345a6917539553cf690797e339cb6f8
DiegoRomario <diego.romario@outlook.com>
2025-08-05 07:07:19 -0300

    https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element

---
+++ src/leetcode/61-80/73LongestSubarray.cs
@@ -0,0 +1,28 @@
+﻿internal static partial class Problem
+{
+    public static int LongestSubarray(int[] nums)
+    {
+        int zeroes = 0;
+        int max = 0;
+        int lo = 0;
+
+        for (int hi = 0; hi < nums.Length; hi++)
+        {
+            if (nums[hi] == 0)
+                zeroes++;
+
+            while (zeroes > 1)
+            {
+                if (nums[lo] == 0)
+                    zeroes--;
+
+                lo++;
+            }
+
+            int currMax = hi - lo;
+            max = Math.Max(max, currMax);
+        }
+
+        return max;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,2 @@
-﻿Problem.LongestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2);
+﻿Problem.LongestSubarray([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]);
 Console.ReadKey();
