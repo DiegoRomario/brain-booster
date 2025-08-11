abddb390060b1759fa4fb3501a2bc8df04c33a96
DiegoRomario <diego.romario@outlook.com>
2025-08-11 06:41:27 -0300

    https://leetcode.com/problems/number-of-recent-calls

---
+++ src/leetcode/81-100/81NumberOfRecentCalls.cs
@@ -0,0 +1,47 @@
+﻿internal static partial class Problem
+{
+    public class RecentCounter
+    {
+        private readonly Queue<int> q;
+
+        public RecentCounter()
+        {
+            q = [];
+        }
+
+        public int Ping(int t)
+        {
+            q.Enqueue(t);
+            while (q.Peek() < t - 3000)
+            {
+                q.Dequeue();
+            }
+            return q.Count;
+        }
+    }
+
+    public class RecentCounterWithOutQueue
+    {
+        private List<int> _list;
+        private int _count;
+        private int _start;
+        public RecentCounterWithOutQueue()
+        {
+            _list = [];
+            _count = 0;
+            _start = 0;
+        }
+        public int Ping(int t)
+        {
+            _list.Add(t);
+            _count++;
+            while (_list[_start] < t - 3000)
+            {
+                _start++;
+                _count--;
+            }
+            return _count;
+        }
+    }
+
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,6 @@
-﻿Problem.GroupThePeople([3, 3, 3, 3, 3, 1, 3]);
+﻿var counter = new Problem.RecentCounter();
+Console.WriteLine(counter.Ping(1));
+Console.WriteLine(counter.Ping(100));
+Console.WriteLine(counter.Ping(3001));
+Console.WriteLine(counter.Ping(3002));
 Console.ReadKey();
