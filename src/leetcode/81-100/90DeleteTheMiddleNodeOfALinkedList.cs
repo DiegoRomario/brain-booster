e52e1b7649ef518b70f7d46959aea0803dc37242
DiegoRomario <diego.romario@outlook.com>
2025-08-18 07:42:41 -0300

    https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list

---
+++ src/leetcode/81-100/90DeleteTheMiddleNodeOfALinkedList.cs
@@ -0,0 +1,40 @@
+﻿internal static partial class Problem
+{
+    public static ListNode DeleteMiddle(ListNode head)
+    {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        if (head == null || head.next == null)
+            return null;
+
+        while (fast != null && fast.next != null)
+        {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = slow.next;
+
+        return head;
+    }
+
+    public static ListNode DeleteMiddleWithOutPrev(ListNode head)
+    {
+        var slow = head;
+        if (head == null || head.next == null) return null;
+
+        ListNode fast = head.next.next;
+
+        while (fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,2 +1,6 @@
-﻿Problem.AsteroidCollision([5, 10, -5]);
+﻿ListNode node4 = new (9);
+ListNode node3 = new (1) { next = node4 };
+ListNode node2 = new (5) { next = node3 };
+ListNode head = new (4) { next = node2 };
+Problem.DeleteMiddle(head);
 Console.ReadKey();
