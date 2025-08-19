ea17f5495e93790ca6af3c5f690d4814a953f374
DiegoRomario <diego.romario@outlook.com>
2025-08-19 08:13:55 -0300

    https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list

---
+++ src/leetcode/81-100/92MaximumTwinSumOfALinkedList.cs
@@ -0,0 +1,66 @@
+﻿internal static partial class Problem
+{
+    public static int PairSum(ListNode head)
+    {
+        // 1. Find the middle
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // 2. Reverse second half (starting from slow, which is the middle point)
+        ListNode prev = null, curr = slow;
+        while (curr != null)
+        {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        // 3. Compare from both ends
+        int answer = 0;
+        ListNode left = head, right = prev;
+        while (right != null)
+        {
+            answer = Math.Max(answer, left.val + right.val);
+            left = left.next;
+            right = right.next;
+        }
+
+        return answer;
+    }
+
+    public static int PairSumFaster (ListNode head)
+    {
+        // Reverse first half while finding the midpoint
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+
+            // Reverse step
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // prev = head of reversed first half
+        // curr = head of second half
+        int answer = 0;
+        while (curr != null)
+        {
+            answer = Math.Max(answer, curr.val + prev.val);
+            curr = curr.next;
+            prev = prev.next;
+        }
+
+        return answer;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,7 +1,6 @@
-﻿ListNode node5 = new(5);
-ListNode node4 = new (4) { next = node5};
+﻿ListNode node4 = new (4);
 ListNode node3 = new (3) { next = node4 };
 ListNode node2 = new (2) { next = node3 };
 ListNode head = new (1) { next = node2 };
-Problem.OddEvenList(head);
+Problem.PairSum(head);
 Console.ReadKey();
