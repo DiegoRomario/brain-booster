52a56774a88ab8a594fc84bd4c954e48595a8466
DiegoRomario <diego.romario@outlook.com>
2025-08-19 06:53:14 -0300

    https://leetcode.com/problems/odd-even-linked-list

---
+++ src/leetcode/81-100/91OddEvenLinkedList.cs
@@ -0,0 +1,23 @@
+﻿internal static partial class Problem
+{
+    public static ListNode OddEvenList(ListNode head)
+    {
+        if (head == null || head.next == null) return head;
+
+        var odd = head;
+        var even = head.next;
+        var even_h = even;
+
+        while (even != null && even.next != null)
+        {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = even_h;
+
+        return head;
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -1,6 +1,7 @@
-﻿ListNode node4 = new (9);
-ListNode node3 = new (1) { next = node4 };
-ListNode node2 = new (5) { next = node3 };
-ListNode head = new (4) { next = node2 };
-Problem.DeleteMiddle(head);
+﻿ListNode node5 = new(5);
+ListNode node4 = new (4) { next = node5};
+ListNode node3 = new (3) { next = node4 };
+ListNode node2 = new (2) { next = node3 };
+ListNode head = new (1) { next = node2 };
+Problem.OddEvenList(head);
 Console.ReadKey();
