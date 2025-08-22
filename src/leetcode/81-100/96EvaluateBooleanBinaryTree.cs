c5f5de77f30e4f9d23fbec944bcaa651f9315d09
DiegoRomario <diego.romario@outlook.com>
2025-08-22 06:31:59 -0300

    https://leetcode.com/problems/evaluate-boolean-binary-tree

---
+++ src/leetcode/81-100/96EvaluateBooleanBinaryTree.cs
@@ -0,0 +1,12 @@
+﻿internal static partial class Problem
+{
+    public static bool EvaluateTree(TreeNode root)
+    {
+        if (root.left is null) return root.val == 1;
+
+        if (root.val == 2)
+            return (EvaluateTree(root.left) || EvaluateTree(root.right));
+        else
+            return (EvaluateTree(root.left) && EvaluateTree(root.right));
+    }
+}
--- src/leetcode/Program.cs
+++ src/leetcode/Program.cs
@@ -2,19 +2,19 @@
 
 
 TreeNode root = new(3,
-    new TreeNode(5,
-        new TreeNode(6),
+    new TreeNode(2,
+        new TreeNode(1),
         new TreeNode(2,
-            new TreeNode(7),
-            new TreeNode(4)
+            new TreeNode(1),
+            new TreeNode(0)
         )
     ),
-    new TreeNode(1,
-        new TreeNode(9),
-        new TreeNode(8)
+    new TreeNode(2,
+        new TreeNode(1),
+        new TreeNode(0)
     )
 );
 
 
-LeafSimilar(root, root);
+EvaluateTree(root);
 Console.ReadKey();
